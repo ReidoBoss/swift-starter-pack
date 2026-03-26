2f5b6e892e360fa1dc4b47dbed98fd33fdfcaed0
ReidoBoss <bryansagarino222@gmail.com>
2026-03-27 00:10:07 +0800

    Initial Commit

---
+++ SwiftStarter/SwiftStarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftStarterApp.swift
+//  SwiftStarter
+//
+//  Created by Stephen Sagarino on 3/27/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftStarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
